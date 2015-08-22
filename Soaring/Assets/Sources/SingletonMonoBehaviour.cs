a0b173d8dda3e7c7a5552d5adc1f177a9599feb4
isam hamaguchi <spacebeagle2014@gmail.com>
2015-08-22 17:07:13 +0900

    3dPersonｺﾝﾄﾛｰﾗ

---
+++ Soaring/Assets/Sources/SingletonMonoBehaviour.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Singleton mono behaviour.
+/// </summary>
+public class SingletonMonoBehaviour<T> : MonoBehaviour where T : MonoBehaviour {
+    private static GameObject singletonGameObject;
+    private static T instance;
+    public static T Instance {
+        get {
+            if (instance == null) {
+                instance = FindObjectOfType<T>();
+
+                if (instance == null) {
+                    if (singletonGameObject == null) {
+                        singletonGameObject = GameObject.Find("SingletonGameObject");
+                        if (singletonGameObject == null) {
+                            singletonGameObject = new GameObject("SingletonGameObject");
+                            if (singletonGameObject == null) {
+                                Debug.LogError(typeof(T) + "is nothing");
+                                return null;
+                            }
+                        }
+                    }
+                    instance = singletonGameObject.AddComponent<T>();
+                    if (instance == null) {
+                        Debug.LogError(typeof(T) + "is nothing");
+                        return null;
+                    }
+                    singletonGameObject = instance.gameObject;
+                    DontDestroyOnLoad(singletonGameObject);
+                }
+            }
+            return instance;
+        }
+    }
+
+    protected void Awake() {
+        if (instance == null) {
+            instance = this as T;
+            return;
+        } else if (Instance == this) {
+            return;
+        }
+        Destroy(this);
+        return;
+    }
+}
