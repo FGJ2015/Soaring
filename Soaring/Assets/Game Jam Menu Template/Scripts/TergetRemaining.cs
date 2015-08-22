9e6da8f561489e06cc85dd17d74b6f7f3831ca6a
Imaizumi Takayosi <takayosi@Imaizumi>
2015-08-22 23:27:30 +0900

    背景変更　ノルマ表示

---
+++ Soaring/Assets/Game Jam Menu Template/Scripts/TergetRemaining.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TergetRemaining : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+     
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   GetComponent<Text>().text = "ノルマ;" + GameObject.FindGameObjectsWithTag("Terget").Length;
+	}
+}
