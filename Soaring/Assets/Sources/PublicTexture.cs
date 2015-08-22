b734da03ff37f1d54118bd64b52c6509f835e77e
isam hamaguchi <spacebeagle2014@gmail.com>
2015-08-23 03:23:06 +0900

    テクスチャ簡単獲得

---
+++ Soaring/Assets/Sources/PublicTexture.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+[RequireComponent(typeof(SetTexture))]
+public class PublicTexture : SingletonMonoBehaviour<PublicTexture> {
+    private SetTexture m_setTexture;
+	// Use this for initialization
+	void Awake () {
+        m_setTexture = GetComponent<SetTexture>();	
+	}
+
+    static public Texture2D GetTexture(int index,string place)
+    {
+        return Instance.m_setTexture.GetTexture(index,place) as Texture2D;
+    }
+
+    static int armCount = 0;
+    static public Texture2D GetTextureArm()
+    {
+        if (!Instance.m_setTexture.g_bFlag_Assets || Instance.m_setTexture.g_arAssets_Arm.Count<=0)
+            return null;
+        armCount = armCount % Instance.m_setTexture.g_arAssets_Arm.Count;
+        Texture2D tx=Instance.m_setTexture.GetTexture(armCount,"arm") as Texture2D;
+        if (tx == null)
+            return tx;
+        armCount++;
+        return tx;
+    }
+
+    static int bodyCount = 0;
+    static public Texture2D GetTextureBody()
+    {
+        if (!Instance.m_setTexture.g_bFlag_Assets || Instance.m_setTexture.g_arAssets_Body.Count<=0)
+            return null;
+        bodyCount = bodyCount % Instance.m_setTexture.g_arAssets_Body.Count;
+        Texture2D tx = Instance.m_setTexture.GetTexture(bodyCount,"body") as Texture2D;
+        if (tx == null)
+            return tx;
+        bodyCount++;
+        return tx;
+    }
+}
