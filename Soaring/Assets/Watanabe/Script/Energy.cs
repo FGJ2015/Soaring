d1bcfae1a85584afe2b082e886f5d864a803bf9d
watanabe_s1200070 <sbut4s9i@watabe-no-MacBook-Pro.local>
2015-08-23 02:52:22 +0900

    エネルギー

---
+++ Soaring/Assets/Watanabe/Script/Energy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Energy : MonoBehaviour {
+	bool flg = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider col){
+		if(flg == false){
+		if(col.gameObject.tag=="Player"){
+			Event();
+			flg = true;
+		}
+	 	}
+		Destroy(this.gameObject);
+	}
+
+	//This event occers when player hits energy object
+	void Event(){
+		Player_ItemGet.Point++;
+	}
+
+}
---
+++ Soaring/Assets/Watanabe/Script/Player_ItemGet.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_ItemGet: MonoBehaviour {
+
+	public static int Point;
+
+	// Use this for initialization
+	void Start () {
+		Point = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log(Point);
+	}
+}
